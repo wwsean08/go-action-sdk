8a0754487d6fca6816e35553d04d859db1568cb4
Sean Smith <sean@wwsean08.com>
2017-04-22 10:18:08 -0700

    Add some unit tests around the AskResponse* functions more unit tests/conditions to be checked around the NoInputResponses also need to do functional testing at this point.

--- ResponseBuilder_test.go
+++ ResponseBuilder_test.go
@@ -28,3 +28,41 @@ func TestDefaultResponse_TellResponseSSMLReturnsExpectedOutput(t *testing.T) {
 	assert.Equal(t, &message, response.FinalResponse_.SpeechResponse_.SSML)
 	assert.Nil(t, response.FinalResponse_.SpeechResponse_.TextToSpeech)
 }
+
+func TestDefaultResponse_AskResponseReturnsExpectedOutput(t *testing.T) {
+	// nil noInputPrompt check
+	builder := NewResponseBuilder()
+	cToken := "42"
+	message := "This Is A Test"
+	response := builder.AskResponse(message, &cToken, nil)
+	assert.True(t, response.ExpectUserResponse)
+	assert.Nil(t, response.FinalResponse_)
+	assert.NotNil(t, response.ExpectedInputs)
+	assert.Len(t, response.ExpectedInputs, 1)
+	assert.Len(t, response.ExpectedInputs[0].PossibleIntents, 1)
+	assert.Equal(t, "assistant.intent.action.TEXT", response.ExpectedInputs[0].PossibleIntents[0].Intent)
+	assert.Nil(t, response.ExpectedInputs[0].PossibleIntents[0].InputValueSpec_)
+	assert.Len(t, response.ExpectedInputs[0].InputPrompt_.InitialPrompts, 1)
+	assert.Nil(t, response.ExpectedInputs[0].InputPrompt_.InitialPrompts[0].SSML)
+	assert.Equal(t, &message, response.ExpectedInputs[0].InputPrompt_.InitialPrompts[0].TextToSpeech)
+	assert.Nil(t, response.ExpectedInputs[0].InputPrompt_.NoInputPrompts)
+}
+
+func TestDefaultResponse_AskResponseSSMLReturnsExpectedOutput(t *testing.T) {
+	// nil noInputPrompt check
+	builder := NewResponseBuilder()
+	cToken := "42"
+	message := "This Is A Test"
+	response := builder.AskResponseSSML(message, &cToken, nil)
+	assert.True(t, response.ExpectUserResponse)
+	assert.Nil(t, response.FinalResponse_)
+	assert.NotNil(t, response.ExpectedInputs)
+	assert.Len(t, response.ExpectedInputs, 1)
+	assert.Len(t, response.ExpectedInputs[0].PossibleIntents, 1)
+	assert.Equal(t, "assistant.intent.action.TEXT", response.ExpectedInputs[0].PossibleIntents[0].Intent)
+	assert.Nil(t, response.ExpectedInputs[0].PossibleIntents[0].InputValueSpec_)
+	assert.Len(t, response.ExpectedInputs[0].InputPrompt_.InitialPrompts, 1)
+	assert.Nil(t, response.ExpectedInputs[0].InputPrompt_.InitialPrompts[0].TextToSpeech)
+	assert.Equal(t, &message, response.ExpectedInputs[0].InputPrompt_.InitialPrompts[0].SSML)
+	assert.Nil(t, response.ExpectedInputs[0].InputPrompt_.NoInputPrompts)
+}
